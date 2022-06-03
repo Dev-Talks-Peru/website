d7eef42dc2001f60ea43a847672c838e2a8df631
Sergio Xalambrí <hello@sergiodxa.com>
2022-06-03 14:39:58 -0500

    Change favicon and title

--- app/root.tsx
+++ app/root.tsx
@@ -17,23 +17,24 @@ import { useChangeLanguage } from "remix-i18next";
 import tailwind from "~/styles/tailwind.css";
 import { i18n } from "./services/i18n.server";
 
-type LoaderData = { locale: string };
-
-export let loader: LoaderFunction = async ({ request }) => {
-  let locale = await i18n.getLocale(request);
-  return json<LoaderData>({ locale });
-};
-
-export let meta: MetaFunction = () => {
-  return {
-    charset: "utf-8",
-    title: "New Remix App",
-    viewport: "width=device-width,initial-scale=1",
-  };
-};
+type LoaderData = { locale: string; title: string };
 
 export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwind }];
+};
+
+export let loader: LoaderFunction = async ({ request }) => {
+  let locale = await i18n.getLocale(request);
+  let t = await i18n.getFixedT(locale);
+  return json<LoaderData>({ locale, title: t("communityName") });
+};
+
+export let meta: MetaFunction = ({ data }) => {
+  return {
+    charset: "utf-8",
+    title: data.title,
+    viewport: "width=device-width,initial-scale=1",
+  };
 };
 
 export let handle = { i18n: "translation" };
@@ -49,6 +50,7 @@ export default function App() {
     <html lang={locale} dir={i18n.dir()} className="bg-gray-100 text-orange-10">
       <head>
         <Meta />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <Links />
       </head>
       <body>
